ff42e8b1d98fae81747ae1cef8dca74b8754d38e
Dari H <2630040515@qq.com>
2024-09-02 09:00:42 +0800

    3 sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://ai-headershot.sora.makeup/', // 替换为你的实际网站URL
+  generateRobotsTxt: true, // 是否生成robots.txt文件
+  sitemapSize: 5000, // 可选，定义每个sitemap的最大URL数量
+  exclude: ['/secret-page'], // 可选，排除特定页面
+  changefreq: 'daily', // 可选，设置页面更新频率
+  priority: 0.7, // 可选，设置页面的优先级，1.0为最高优先级
+  outDir: 'out', // 输出目录
+};
